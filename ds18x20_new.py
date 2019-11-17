291ccd7ea03b38172f22124719ffc51cd993a065
robert <robert@hammelrath.com>
2019-11-17 15:33:47 +0100

    ds18x20.py: Determine supply mode during init
    Class variable powermode will be set 1 one in the presence of
    strong power, to 0 in case of parasitic powering.
    
    Also:
    - Harmonize naming of constants
    - check the rom id for both 0x22 and 0x28 at read_temp

--- ds18x20_new.py
+++ ds18x20_new.py
@@ -3,15 +3,18 @@
 
 from micropython import const
 
-_CONVERT = const(0x44)
-_RD_SCRATCH = const(0xbe)
-_WR_SCRATCH = const(0x4e)
+CMD_CONVERT = const(0x44)
+CMD_RDSCRATCH = const(0xbe)
+CMD_WRSCRATCH = const(0x4e)
+CMD_RDPOWER = const(0xb4)
 
 class DS18X20:
     def __init__(self, onewire):
         self.ow = onewire
         self.buf = bytearray(9)
-        self.roms = self.scan()
+        self.ow.writebyte(self.ow.CMD_SKIPROM)
+        self.ow.writebyte(CMD_RDPOWER)
+        self.powermode = self.ow.readbit()
 
     def scan(self):
         return [rom for rom in self.ow.scan() if rom[0] in (0x10, 0x22, 0x28)]
@@ -19,12 +22,12 @@ class DS18X20:
     def convert_temp(self):
         self.ow.reset()
         self.ow.writebyte(self.ow.CMD_SKIPROM)
-        self.ow.writebyte(_CONVERT)
+        self.ow.writebyte(CMD_CONVERT)
 
     def read_scratch(self, rom):
         self.ow.reset()
         self.ow.select_rom(rom)
-        self.ow.writebyte(_RD_SCRATCH)
+        self.ow.writebyte(CMD_RDSCRATCH)
         self.ow.readinto(self.buf)
         assert self.ow.crc8(self.buf) == 0, 'CRC error'
         return self.buf
@@ -32,7 +35,7 @@ class DS18X20:
     def write_scratch(self, rom, buf):
         self.ow.reset()
         self.ow.select_rom(rom)
-        self.ow.writebyte(_WR_SCRATCH)
+        self.ow.writebyte(CMD_WRSCRATCH)
         self.ow.write(buf)
 
     def read_temp(self, rom):
@@ -45,10 +48,12 @@ class DS18X20:
                 else:
                     t = buf[0] >> 1
                 return t - 0.25 + (buf[7] - buf[6]) / buf[7]
-            else:
+            elif rom[0] in (0x22, 0x28):
                 t = buf[1] << 8 | buf[0]
                 if t & 0x8000: # sign bit set
                     t = -((t ^ 0xffff) + 1)
-            return t / 16
+                return t / 16
+            else:
+                return None
         except AssertionError:
             return None
