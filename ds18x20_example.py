9303b2f94447842b872e8cd191c5c52cffe1a17a
robert <robert@hammelrath.com>
2019-11-17 12:20:14 +0100

    ds18x20_slim.py: Port of the Micropython.org version
    This version is clean and simple, and IMHO the preferred one.

--- ds18x20_example.py
+++ ds18x20_example.py
@@ -1,16 +1,17 @@
 import time
 from machine import Pin
-from ds18x20 import DS18X20
+from ds18x20_slim import DS18X20
 from onewire import OneWire
 
 #DS18B20 data line connected to pin P10
 ow = OneWire(Pin('P10'))
 temp = DS18X20(ow)
 
+temp.convert_temp()
 while True:
-    for rom in temp.roms:
-        temp.start_conversion(rom)
-        time.sleep(1)
-        print(temp.read_temp_async(rom), end=" ")
-    print()
     time.sleep(1)
+    for rom in temp.roms:
+        print(temp.read_temp(rom), end=" ")
+    print()
+    temp.convert_temp()
+
